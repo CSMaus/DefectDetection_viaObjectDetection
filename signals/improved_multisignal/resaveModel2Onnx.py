9bc155a75f34dba2988239076bc2c913e0caf2ac
Kseniia Barashok <cs-maus-1@mail.ru>
2025-06-20 18:14:03 +0900

    convert to onnx

---
+++ signals/improved_multisignal/resaveModel2Onnx.py
@@ -0,0 +1,50 @@
+from improved_model import ImprovedMultiSignalClassifier
+import torch
+import os
+
+def export_model_to_onnx(model, device, model_path, onnx_model_path, signal_length, hidden_sizes, num_heads=4):
+    checkpoint = torch.load(model_path, map_location=device)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    model.eval()
+
+
+    dummy_input = torch.randn(1, 50, signal_length).to(device)
+    torch.onnx.export(
+        model,
+        dummy_input,
+        onnx_model_path,
+        export_params=True,
+        opset_version=13,
+        input_names=['input'],
+        output_names=['defect_prob', 'defect_start', 'defect_end'],
+        dynamic_axes={
+            'input': {0: 'batch_size', 1: 'num_signals'},
+            'defect_prob': {0: 'batch_size', 1: 'num_signals'},
+            'defect_start': {0: 'batch_size', 1: 'num_signals'},
+            'defect_end': {0: 'batch_size', 1: 'num_signals'},
+        }
+    )
+
+    print(f"Model exported to {onnx_model_path}")
+
+signal_length = 320
+hidden_sizes = [128, 64, 32]
+num_heads = 8
+dropout = 0.2
+num_transformer_layers = 4
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+
+model = ImprovedMultiSignalClassifier(
+        signal_length=signal_length,
+        hidden_sizes=hidden_sizes,
+        num_heads=num_heads,
+        num_transformer_layers=num_transformer_layers
+    ).to(device)
+
+modelname = "ImprovedMSC"
+attempt = "000"
+model_path = f'models/improved_model_20250615_193609/best_model.pth'
+export_model_to_onnx(model, device, model_path,
+                     f'models/{attempt}-{modelname}.onnx', signal_length, hidden_sizes)
