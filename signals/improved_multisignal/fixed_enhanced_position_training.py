ea5d0c16dbf11b7e3ab2b28ac229b6996ba3d88a
Kseniia Barashok <cs-maus-1@mail.ru>
2025-07-16 20:39:41 +0900

    training

--- signals/improved_multisignal/fixed_enhanced_position_training.py
+++ signals/improved_multisignal/fixed_enhanced_position_training.py
@@ -464,7 +464,7 @@ def main():
     learning_rate = 0.0008
     weight_decay = 0.01
     
-    json_dir = "json_data/"
+    json_dir = "json_data_all/"  # "json_data_07/"  # "json_data/"
     timestamp = datetime.now().strftime("%Y%m%d_%H%M")
     save_dir = f"models/fixed_enhanced_position_model_{timestamp}"
     
