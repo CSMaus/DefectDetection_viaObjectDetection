049faec293eb67cfdac937f00b78310715e56802
Kseniia Barashok <cs-maus-1@mail.ru>
2025-11-12 19:17:08 +0900

    13 model is kinda good

--- signals/improved_multisignal/convert_hybrid_onnx.py
+++ signals/improved_multisignal/convert_hybrid_onnx.py
@@ -3,7 +3,8 @@ from detection_models.hybrid_binary import HybridBinaryModel
 import torch
 import os
 
-def export_model_to_onnx(model, device, model_path, onnx_model_path, signal_length, hidden_sizes, num_heads=8):
+def export_model_to_onnx(model, device, model_path, onnx_model_path, signal_length):
+    # , hidden_sizes, num_heads=8):
     checkpoint = torch.load(model_path, map_location=device)
     model.load_state_dict(checkpoint['model_state_dict'])
     model.eval()
@@ -29,9 +30,9 @@ def export_model_to_onnx(model, device, model_path, onnx_model_path, signal_leng
     print(f"Model exported to {onnx_model_path}")
 
 signal_length = 320
-hidden_sizes = [128, 64, 32]
+# hidden_sizes = [128, 64, 32]
 num_heads = 8
-dropout = 0.1
+# dropout = 0.05  # 0.1
 num_transformer_layers = 4
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -39,14 +40,14 @@ print(f"Using device: {device}")
 
 model = HybridBinaryModel(
     signal_length=signal_length,
-    hidden_sizes=hidden_sizes,
+    # hidden_sizes=hidden_sizes,
     num_heads=num_heads,
-    dropout=dropout,
+    # dropout=dropout,
     num_transformer_layers=num_transformer_layers
 ).to(device)
 
-modelname = "HybridBinaryModel_pepero"
-attempt = "005"
-model_path = f'models/HybridBinaryModel_20251111_2129/best_detection.pth'  # UPDATE THIS PATH
+modelname = "HybridBinaryModel"
+attempt = "013"
+model_path = f'models/HybridBinaryModel_20251112_1902/best_detection.pth'  # UPDATE THIS PATH
 export_model_to_onnx(model, device, model_path,
-                     f'models/{attempt}-{modelname}.onnx', signal_length, hidden_sizes, num_heads)
+                     f'models/{attempt}-{modelname}.onnx', signal_length) # , hidden_sizes, num_heads)
